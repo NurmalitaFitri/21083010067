e7c55b6c7b28b17058c4916d59872a8f20d1d4ea
Nurmalita Fitri Ramadani <21083010067@student.upnjatim.ac.id>
2022-10-12 19:07:03 +0700

    Array_CA.sh

---
+++ Array_CA.sh
@@ -0,0 +1,7 @@
+distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'LinuxMint')
+distroLinuxServer=('UbuntuServer' 'CentOS' 'FedoraServer')
+
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
+
+
